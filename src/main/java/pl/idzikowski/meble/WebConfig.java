c7239b4b7e8c37a17a7032b7911d8b004506a1dd
Kacper Idzikowski <kacper.idzikowski@o2.pl>
2019-03-24 19:17:08 +0100

    Add tag icon and add resources tag

--- src/main/java/pl/idzikowski/meble/WebConfig.java
+++ src/main/java/pl/idzikowski/meble/WebConfig.java
@@ -1,16 +1,18 @@
 package pl.idzikowski.meble;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+ 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/images/**")
-			.addResourceLocations("file:images/");		
+		registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+		registry.addResourceHandler("/**").addResourceLocations("file:src/main/resources/static/");
 	}
 
 }
